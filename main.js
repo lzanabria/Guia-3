f8faaebee4fa1faa496ace68c8e583883627571f
Luis Zanabria - ADSI <lazanabria2@misena.edu.co>
2022-09-29 08:51:32 -0500

    Ejercicio 4

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let cant, nom, ape, edad, nota1, nota2, nota3, prom, estado="";
-        let cont=1;
-        cant=Number(prompt("Ingrese la cantidad de estudiantes: ",5));
-        while(cont<=cant){
-            nom=prompt("Estudiante #"+cont+"\n\tIngrese el nombre del estudiante: ");
-            ape=prompt("Estudiante #"+cont+"\n\tIngrese el apellido del estudiante: ");
-            edad=Number(prompt("Estudiante #"+cont+"\n\tIngrese la edad del estudiante: "));
-            nota1=Number(prompt("Estudiante #"+cont+"\n\tIngrese la primera nota del estudiante: "));
-            nota2=Number(prompt("Estudiante #"+cont+"\n\tIngrese la segunda nota del estudiante: "));
-            nota3=Number(prompt("Estudiante #"+cont+"\n\tIngrese la tercera nota del estudiante: "));
-            prom=(nota1+nota2+nota3)/3;
-
-            if(prom>=7){
-                estado="Aprobado";
-            }
-            else{
-                estado="Reprobado";
-            }
-            alert("▶ Estudiante: "+nom+" "+ape+"\n▶ Edad: "+edad+"\n\t▶ Nota 1: "+nota1+"\n\t▶ Nota 2: "+nota2+"\n\t▶ Nota 3: "+nota3+"\n▶ Promedio: "+prom+"\n▶ Estado: "+estado);
-            cont++;
+        let damas=1, caballeros=7, personas=0;
+        while(personas<42) {
+            damas++;
+            caballeros++;
+            personas=damas+caballeros;
         }
+        alert("\tNúmero de damas: "+damas+"\n\tNúmero de caballeros: "+caballeros+"\n\tTotal de personas: "+personas);
     }, 500);
 });
