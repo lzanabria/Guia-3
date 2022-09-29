7715443dc48c6d23fc1b9356c3ab13098878ea81
Luis Zanabria - ADSI <lazanabria2@misena.edu.co>
2022-09-29 11:12:10 -0500

    Ejercicio 6

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let cant, alum, nota, nota_acum=0, prom, niv=0, no_niv=0;
-        let cont=1, unidades=1;
-        cant=Number(prompt("Ingrese la cantidad de alumnos: ",40));
-        while(cont<=cant) {
-            alum=prompt("Alumno #"+cont+"\n\tIngrese el nombre del alumno: ");
-            while(unidades<=5) {
-                nota=Number(prompt("Alumno #"+cont+"\n\tIngrese la nota de la unidad "+unidades+": "));
-                nota_acum+=nota;
-                prom=nota_acum/5;
-                unidades++;
-            }
-            if(prom<7) {
-                niv++;
-            }
-            else {
-                no_niv++;
-            }
-            cont++;
-            unidades=1;
+        let segundos=0;
+        let avion=20;
+        let misil=10;
+        while(avion-misil <= 10000) {
+            segundos++;
+            avion+=20+avion;
+            misil+=10+misil;
+            alert("En "+segundos+" segundos la distancia del avion es "+avion+" y la del misil es "+misil);
         }
-        alert("▶ Número de alumnos totales: "+cant+"\n\tAlumnos que deben nivelar: "+niv+"\n\tAlumnos que no deben nivelar: "+no_niv);
     }, 500);
 });
