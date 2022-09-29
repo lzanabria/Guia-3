34ba48d6adcc8c5b74472a31a9801bcb8ed5056c
Luis Zanabria - ADSI <lazanabria2@misena.edu.co>
2022-09-29 08:22:19 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let n_gallina, peso, altura, n_huevos, calidad, prom_calidad, precio;
-        let cont=1, calidad_acum=0;
-
-        n_gallina=Number(prompt("Ingrese el número de gallinas: "));
-        while(cont<=n_gallina){
-            peso=Number(prompt("Gallina #"+cont+"\n▶ Ingrese el peso de la gallina (kg): "));
-            altura=Number(prompt("Gallina #"+cont+"\n▶ Ingrese la altura de la gallina (cm): "));
-            n_huevos=Number(prompt("Gallina #"+cont+"\n▶ Ingrese el número de huevos que pone la gallina: ")); 
-            calidad=(peso*altura)/n_huevos;
-            calidad_acum+=calidad;
+        let cant, n1, n2, sum, res, mult, div;
+        let cont=1;
+        cant=Number(prompt("Ingrese la cantidad de procesos que desea realizar: ", 10));
+        while(cont<=cant) {
+            n1=Number(prompt("Proceso #"+cont+"\n▶ Ingrese el valor del primer número: "));
+            n2=Number(prompt("Proceso #"+cont+"\n▶ Ingrese el valor del segundo número: "));
+            sum=n1+n2;
+            res=n1-n2;
+            mult=n1*n2;
+            div=n1/n2;
+            alert("Proceso #"+cont+"\n▶ Suma: "+sum+"\n▶ Resta: "+res+"\n▶ Multiplicación: "+mult+"\n▶ División: "+div);
             cont++;
         }
-        prom_calidad=calidad_acum/n_gallina;
-
-        if(prom_calidad>=15){
-            precio=prom_calidad*1.2;
-        }
-        else if(prom_calidad>8 && prom_calidad<15){
-            precio=prom_calidad*1.0;
-        }
-        else{
-            precio=prom_calidad*0.80;
-        }
-        alert("▶ La calidad del huevo es: "+calidad+"\n▶ El precio del kilo de huevo es: $"+precio);
     }, 500);
 });
